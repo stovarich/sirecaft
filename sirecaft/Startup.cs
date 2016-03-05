824e7f94cec1a6349e533b1ef00ca2a00d2b537b
stovarich <stovarich@gmail.com>
2016-03-05 13:09:26 -0400

    first commit

---
+++ sirecaft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sirecaft.Startup))]
+namespace sirecaft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
